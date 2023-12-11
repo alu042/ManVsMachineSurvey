a9e9a2b5f6c55f4595ba933fe59150dad6a9023f
Sindre Kjelsrud <kjelsrudsindre@gmail.com>
2023-12-11 19:03:57 +0100

    :construction: started working on landingpage
    Co-authored-by: haraldnilsen <harald_998@hotmail.com>

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,8 +1,24 @@
-<h1 class="text-xl">Velkommen til Helseveileder</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-
-<style lang="postcss">
-    :global(html) {
-      background-color: theme(colors.gray.300);
-    }
-</style>
+<div class="flex flex-col justify-center">
+    <div class="flex">
+        <div class="w-3/5">
+            <h1 class="text-6xl text-primary">Helseveileder</h1>
+            <p>
+                Vi utfører en undersøkelse om kunstig intelligens (AI) i medisin. Vi ønsker å forstå hvordan AI kan fungere som en copilot for leger og pasienter.
+                Undersøkelsen er helt anonym og informasjonen vil bli brukt til å forbedre vår tjeneste. 
+            </p>
+            <ul>
+                <p>I denne undersøkelsen vil vi at du skal vurdere følgende: </p>
+                <li>Empati: Evnen til AI til å forstå og dele følelsene til en annen.</li>
+                <li>Kunnskap: Nøyaktighet og relevans av den medisinske informasjonen gitt.</li>
+                <li>Hjelpsomhet: Evnen til å gi nyttig og praktisk råd eller støtte.</li>
+            </ul>
+        </div>
+        <div class="flex justify-center items-center w-2/5">
+            <button><a href="/form">Start undersøkelse</a></button>
+        </div>
+    </div>
+    <div class="flex justify-center items-center gap-8 text-primary">
+        <button>Personvern</button>
+        <button>Mer info</button>
+    </div>
+</div>
