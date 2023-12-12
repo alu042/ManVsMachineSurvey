4cd2ed9a8ad272eb398b886d042d2a66b38fa9f4
haraldnilsen <harald_998@hotmail.com>
2023-12-12 20:36:17 +0100

    :sparkles: added new form page
    Co-authored-by: Sindre Kjelsrud <kjelsrudsindre@gmail.com>

--- frontend/src/components/userform/inputs/RadioButtons.svelte
+++ frontend/src/components/userform/inputs/RadioButtons.svelte
@@ -2,10 +2,10 @@
     export let options:string[]
 </script>
 
-<div class="flex justify-between">
+<div class="flex justify-between w-full">
 {#each options as data, index (data)}
-    <div class="flex flex-col justify-center items-center w-24 ">
-        <button name={data} class="h-7 w-7 rounded-full border-2 border-primary hover:bg-primary"></button>
+    <div class="flex flex-col justify-start items-center  w-12 text-center">
+        <button name={data} class="h-6 w-6 rounded-full border-2 border-primary hover:bg-primary"></button>
         <label class="text-primary text-sm mt-1" for={data}>{data}</label>
     </div>
 {/each}
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,15 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{html,js,svelte,ts}"],
-  theme: {
-    extend: {
-      colors: {
-        primary: "#08667C",
-        secondary: "#CED9DD",
-        content: "#121212",
-        bg: "#EEF0F3",
-      },
-    },
-  },
-  plugins: [],
+	content: ["./src/**/*.{html,js,svelte,ts}"],
+	theme: {
+		extend: {
+			colors: {
+				primary: "#08667C",
+				secondary: "#CED9DD",
+				content: "#121212",
+				bg: "#EEF0F3",
+				error: "#964747",
+			},
+		},
+	},
+	plugins: [],
 };
