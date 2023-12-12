9aa16763c6b732483bc919513d82b00c6dcaeedc
haraldnilsen <harald_998@hotmail.com>
2023-12-12 19:37:40 +0100

    :bento: svg component for arrow back
    Co-authored-by: Sindre Kjelsrud <kjelsrudsindre@gmail.com>

---
+++ frontend/src/components/svg/ArrowBack.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    export let width:string
+</script>
+
+<svg width={width} height={width} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+
+<g id="SVGRepo_bgCarrier" stroke-width="0"/>
+
+<g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"/>
+
+<g id="SVGRepo_iconCarrier"> <path d="M8 17L3 12M3 12L8 7M3 12H21" stroke="#08667C" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/> </g>
+
+</svg>
