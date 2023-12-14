f52177198d8a1cdea21a2b392e24455cea2793d7
Sindre Kjelsrud <kjelsrudsindre@gmail.com>
2023-12-14 11:30:24 +0100

    :sparkles: Added pages "Personvern" & "Mer info"
    Co-authored-by: haraldnilsen <harald_998@hotmail.com>
    Signed-off-by: Sindre Kjelsrud <kjelsrudsindre@gmail.com>

---
+++ frontend/src/routes/info/+page.svelte
@@ -0,0 +1,28 @@
+<script>
+	import ButtonComponent from "../../components/ButtonComponent.svelte";
+
+</script>
+<div class="flex flex-col justify-between items-center h-full">
+    <div class="flex h-full mt-8">
+        <div class="flex flex-col gap-4 px-64 items-center">
+            <h1 class="text-3xl text-primary font-bold">Info</h1>
+            <p>Vi utfører en undersøkelse om kunstig intelligens (AI) i medisin. Vi ønsker å forstå hvordan AI kan fungere som en copilot for leger og pasienter.</p>
+        </div>
+    </div>
+    <div class="flex justify-center items-center gap-8 text-primary font-bold">
+        <button><a href="/">Tilbake</a></button>
+    </div>
+</div>
+
+<style lang="postcss">
+    :root {
+        color: theme(colors.content);
+        
+    }
+    p {
+        font-size: large;
+    }
+    button {
+        font-size: large;
+    }
+</style>
---
+++ frontend/src/routes/personvern/+page.svelte
@@ -0,0 +1,28 @@
+<script>
+	import ButtonComponent from "../../components/ButtonComponent.svelte";
+
+</script>
+<div class="flex flex-col justify-between items-center h-full">
+    <div class="flex h-full mt-8">
+        <div class="flex flex-col gap-4 px-64 items-center">
+            <h1 class="text-3xl text-primary font-bold">Personvern</h1>
+            <p>Undersøkelsen er helt anonym og informasjonen vil bli brukt til å forbedre vår tjeneste.</p>
+        </div>
+    </div>
+    <div class="flex justify-center items-center gap-8 text-primary font-bold">
+        <button><a href="/">Tilbake</a></button>
+    </div>
+</div>
+
+<style lang="postcss">
+    :root {
+        color: theme(colors.content);
+        
+    }
+    p {
+        font-size: large;
+    }
+    button {
+        font-size: large;
+    }
+</style>
