5a71b084f56c1dde38c2494330a30ac40540bde3
Sindre Kjelsrud <kjelsrudsindre@gmail.com>
2023-12-14 14:48:22 +0100

    :rotating_light: fix compiler warnings
    Co-authored-by: haraldnilsen <harald_998@hotmail.com>
    Signed-off-by: Sindre Kjelsrud <kjelsrudsindre@gmail.com>

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,8 +1,5 @@
 <script lang="ts">
-    import type { LayoutData } from './$types';
     import "../app.css";
-    
-    export let data: LayoutData;
 </script>
 
 <div class="bg-bg w-screen h-screen px-28 py-14">
--- frontend/src/routes/form/+page.svelte
+++ frontend/src/routes/form/+page.svelte
@@ -51,14 +51,14 @@
         </div>
     </div>
     <div class="flex justify-center items-center gap-8">
-        <a href="">
+        <a href="/">
             <button class="flex items-center gap-2 text-primary font-semibold">
                 <ArrowChevron width=16 direction="left"/>
                 Forrige spørsmål
             </button>
         </a>
         <ButtonComponent text="Send inn svar" url="/" filled={true} />
-        <a href="">
+        <a href="/">
             <button class="flex items-center gap-2 text-primary font-semibold opacity-50">
                 Neste spørsmål
                 <ArrowChevron width=16 direction="right"/>
@@ -71,11 +71,6 @@
     :root {
         color: theme(colors.content);
         
-    }
-    li {
-        list-style-type: disc;
-        margin-left: 2rem;
-        font-size: large;
     }
     p {
         font-size: medium;
