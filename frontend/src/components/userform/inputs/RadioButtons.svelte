14c24ae97ee6816e739241e92b9c8b4e5872d983
haraldnilsen <harald_998@hotmail.com>
2024-01-10 20:29:25 +0100

    :lipstick: added svg to checked button
    Co-authored-by: Sindre Kjelsrud <kjelsrudsindre@gmail.com>

--- frontend/src/components/userform/inputs/RadioButtons.svelte
+++ frontend/src/components/userform/inputs/RadioButtons.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
     import { createEventDispatcher } from 'svelte';
+    import CheckMark from '../../svg/CheckMark.svelte';
     const dispatch = createEventDispatcher();
     function handleButtonClick(selectedValue: string) {
         dispatch('update', selectedValue);
@@ -11,7 +12,11 @@
 <div class="flex justify-between w-full">
 {#each options as data, index (data)}
     <button name={data} on:click={() => handleButtonClick(data)} class="flex flex-col justify-start items-center w-12 text-center hover:cursor-pointer">
-        <div class={`${selected == data && "bg-primary"} h-6 w-6 rounded-full border-2 border-primary hover:bg-primary`}></div>
+        <div class={`${selected == data && "bg-primary"} flex justify-center items-center h-6 w-6 rounded-full border-2 border-primary hover:bg-primary`}>
+            {#if selected == data}
+                <CheckMark width=10/>
+            {/if}
+        </div>
         <label class="text-primary text-sm mt-1 hover:cursor-pointer" for={data}>{data}</label>
     </button>
 {/each}
