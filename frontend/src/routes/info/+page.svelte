aabf00e3b33a824089ef5c62c9c1a5c2928ad7a6
Sindre Kjelsrud <kjelsrudsindre@gmail.com>
2024-01-14 16:04:48 +0100

    :speech_balloon: :lipstick: update text & styling
    Co-authored-by: haraldnilsen <harald_998@hotmail.com>
    Signed-off-by: Sindre Kjelsrud <kjelsrudsindre@gmail.com>

--- frontend/src/routes/info/+page.svelte
+++ frontend/src/routes/info/+page.svelte
@@ -2,15 +2,20 @@
     <div class="flex h-full mt-8">
         <div class="flex flex-col gap-4 md:px-96 items-center">
             <h1 class="text-3xl text-primary font-bold">Info</h1>
-            <p>
-                Vi er to medisinstudenter som skriver hovedfagsoppgave om kunstig intelligens innen medisin. Inspirasjonen til dette prosjektet ble hentet fra en artikkel publisert i tidsskriftet JAMA, som undersøker hvordan ChatGPT responderer på kliniske spørsmål, sammenlignet med svar fra leger. Resultatene fra JAMA-artikkelen er lovende og viser at kunstig intelligens kan ha potensiale til å fungere som en co-pilot for leger. 
-            </p>    
-            <p>
-                I denne undersøkelsen vil dere bli presentert for svar, enten fra ChatGPT 4.0 eller legesvar, og disse vurderes ut ifra empati, kunnskap og hjelpsomhet. Resultatene vil bli brukt til å kartlegge hvilket potensiale kunstig intelligens kan ha innen medisinen, samt påpeke mangler til ChatGPT 4.0. Dette vil bli oppsummert i en hovedfagsoppgave ved UiB, men prosjektet kan også ha bredere implikasjoner på sikt.
-            </p>
+            <div class="flex flex-col gap-4">
+                <p>
+                    Vi er to medisinstudenter som skriver hovedfagsoppgave om kunstig intelligens innen medisin. Inspirasjonen til dette prosjektet ble hentet fra en artikkel publisert i tidsskriftet JAMA, som undersøker hvordan ChatGPT responderer på kliniske spørsmål, sammenlignet med svar fra leger. Resultatene fra JAMA-artikkelen er lovende og viser at kunstig intelligens kan ha potensiale til å fungere som en co-pilot for leger. 
+                </p>    
+                <p>
+                    I denne undersøkelsen vil dere bli presentert for svar, enten fra ChatGPT 4.0 eller legesvar, og disse vurderes ut ifra empati, kunnskap og hjelpsomhet. Resultatene vil bli brukt til å kartlegge hvilket potensiale kunstig intelligens kan ha innen medisinen, samt påpeke mangler til ChatGPT 4.0. Dette vil bli oppsummert i en hovedfagsoppgave ved UiB, men prosjektet kan også ha bredere implikasjoner på sikt.
+                </p>
+                <p>
+                    Ønsker du å se hvordan vi har laget denne nettsiden, finner du kildekoden <a href="https://github.com/haraldnilsen/helseveileder" class="text-primary font-semibold">her</a>.
+                </p>
+            </div>
         </div>
     </div>
-    <div class="flex justify-center items-center gap-8 mt-10 md:mt-0 text-primary font-bold">
+    <div class="flex justify-center items-center gap-8 mt-10 md:mt-4 text-primary font-bold">
         <button><a href="/">Tilbake</a></button>
     </div>
 </div>
@@ -18,7 +23,6 @@
 <style lang="postcss">
     :root {
         color: theme(colors.content);
-        
     }
     p {
         font-size: large;
--- frontend/src/routes/personvern/+page.svelte
+++ frontend/src/routes/personvern/+page.svelte
@@ -2,18 +2,20 @@
     <div class="flex h-full mt-8">
         <div class="flex flex-col gap-4 md:px-96 items-center">
             <h1 class="text-3xl text-primary font-bold">Personvern</h1>
-            <p>
-                Vi verdsetter din deltakelse i denne studien. Som en del av vår forpliktelse til å sikre personvern, ønsker vi å informere deg om hvordan vi håndterer dine data.
-            </p>
-            <p>
-                All informasjon du gir vil bli lagret anonymt. Dette betyr at svarene du gir ikke vil kunne spores tilbake til deg.
-            </p>
-            <p>
-                Vi registrerer alder i kohorter (10-20, 21-30, 31-40 osv.), utdanningsnivå, om du er helsepersonell (ja/nei), og kjønn. Dette er for å få en overordnet forståelse av deltakerne i studien. Vi spør ikke om personidentifiserbare opplysninger som navn, fødselsdato, personnummer, e-postadresse, telefonnummer eller geografiske data.
-            </p>
+            <div class="flex flex-col gap-4">
+                <p>
+                    Vi verdsetter din deltakelse i denne studien. Som en del av vår forpliktelse til å sikre personvern, ønsker vi å informere deg om hvordan vi håndterer dine data.
+                </p>
+                <p>
+                    All informasjon du gir vil bli lagret anonymt. Dette betyr at svarene du gir ikke vil kunne spores tilbake til deg.
+                </p>
+                <p>
+                    Vi registrerer alder i kohorter (10-20, 21-30, 31-40 osv.), utdanningsnivå, om du er helsepersonell (ja/nei), og kjønn. Dette er for å få en overordnet forståelse av deltakerne i studien. Vi spør ikke om personidentifiserbare opplysninger som navn, fødselsdato, personnummer, e-postadresse, telefonnummer eller geografiske data.
+                </p>
+            </div>
         </div>
     </div>
-    <div class="flex justify-center items-center gap-8 mt-10 md:mt-0 text-primary font-bold">
+    <div class="flex justify-center items-center gap-8 mt-10 md:mt-4 text-primary font-bold">
         <button><a href="/">Tilbake</a></button>
     </div>
 </div>
