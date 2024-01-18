db05443ed72b919610a2057e595f43a651b4f4bf
Sid <93219711+SindreKjelsrud@users.noreply.github.com>
2024-01-18 16:30:34 +0100

    :speech_balloon: update title

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -4,7 +4,7 @@
 <div class="flex flex-col justify-between items-center h-full md:h-screen w-full">
     <div class="flex flex-col md:flex-row h-full w-full items-center">
         <div class="flex flex-col w-11/12 md:w-3/5 gap-2 md:gap-4">
-            <h1 class="text-5xl md:text-7xl text-primary font-bold pb-2">Helseveileder</h1>
+            <h1 class="text-5xl md:text-7xl text-primary font-bold pb-2">AI i medisin</h1>
             <p>Hvordan responderer ChatGPT på kliniske spørsmål sammenlignet med leger?</p>
             <p>Vi skriver hovedoppgave om kunstig intelligens (AI) i medisin, og ønsker å forstå hvordan AI kan fungere som en co-pilot for leger og pasienter</p>
             <p>Spørsmålene og legesvarene er hentet fra den anonyme nettsiden <a href="https://studenterspør.no" class="text-primary font-bold">Studenter Spør</a>, og er innenfor et spekter av medisinske kategorier. Det er mulig å gå videre til neste spørsmål hvis det er et spørsmål du ikke ønsker å besvare.</p>
